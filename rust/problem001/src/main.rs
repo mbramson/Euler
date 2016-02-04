3454716d5cff462f5ea7531a5ec144eb36c2345e
mbramson <mathewbramson@gmail.com>
2016-02-03 22:27:50 -0500

    add basic io to rust problem001

--- rust/problem001/src/main.rs
+++ rust/problem001/src/main.rs
@@ -4,8 +4,53 @@
 //
 // Find the sum of all the multiples of 3 or 5 below 1000.
 
+use std::io;
+
+fn main() {
+    println!("Please enter first multiple:");
+
+    let mut a = String::new();
+
+    io::stdin().read_line(&mut a)
+               .ok()
+               .expect("Failed to read line");
+
+    let a: u32 = a.trim()
+                  .parse()
+                  .expect("Expected Integer");
+
+    println!("Please enter second multiple:");
+
+    let mut b = String::new();
+
+    io::stdin().read_line(&mut b)
+               .ok()
+               .expect("Failed to read line");
+
+    let b: u32 = b.trim()
+                  .parse()
+                  .expect("Expected Integer");
+
+    println!("Please enter an upper limit:");
+
+    let mut limit = String::new();
+
+    io::stdin().read_line(&mut limit)
+               .ok()
+               .expect("Failed to read line");
+
+    let limit: u32 = limit.trim()
+                          .parse()
+                          .expect("Expected Integer");
+
+    let result: u32 = calculate_sum_of_multiples(a, b, limit);
+
+    println!("The sum of all integers that are multiples of {} or {}", a, b);
+    println!("that are below {} is {}", limit, result);
+}
+
 fn calculate_sum_of_multiples(a: u32, b: u32, limit: u32) -> u32 {
-    let mut total = 0u32;
+    let mut total: u32 = 0;
     // add all multiples of a
 
     let a_n: u32 = (limit - 1)/a;
