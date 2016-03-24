b7cdee44a5a3990d20129e6d7f6546c0a7a4a968
mbramson <mathewbramson@gmail.com>
2016-03-23 22:24:05 -0400

    add sum_of_multiples tests to elixir/problem001

--- elixir/problem001/test/problem001_test.exs
+++ elixir/problem001/test/problem001_test.exs
@@ -2,6 +2,20 @@ defmodule Problem001Test do
   use ExUnit.Case, async: true
   doctest MultipleSummer
 
+  #test "that "
+
+  test "that sum_of_multiples returns 12 for multiples of 3 under 10" do
+    assert MultipleSummer.sum_of_multiples(3, 10) == 18
+  end
+
+  test "that sum_of_multiples returns 0 for multiples of 2 under 1" do
+    assert MultipleSummer.sum_of_multiples(2, 1) == 0
+  end
+
+  test "that sum_of_multiples returns 0 for multiples of 5 under 5" do
+    assert MultipleSummer.sum_of_multiples(5, 5) == 0
+  end
+
   test "that get_iterations returns an integer" do
     n = MultipleSummer.get_iterations(4, 10)
     assert is_integer(n)
@@ -14,6 +28,6 @@ defmodule Problem001Test do
 
   test "that get_iterations is 3 for limit of 12 and n of 3" do
     n = MultipleSummer.get_iterations(3 ,12)
-    assert n == 3, "there should be 3 iterations" 
+    assert n == 3, "there should be 3 iterations"
   end
 end
