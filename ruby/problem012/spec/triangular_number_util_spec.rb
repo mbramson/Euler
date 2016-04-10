2f43fce6024d216bee01376e9037cb39a5433d54
mbramson <mathewbramson@gmail.com>
2016-04-10 13:53:55 -0400

    refactor get_number tests
    add some edge case tests

--- ruby/problem012/spec/triangular_number_util_spec.rb
+++ ruby/problem012/spec/triangular_number_util_spec.rb
@@ -5,16 +5,24 @@ describe TriangularNumberUtil do
   subject(:util) { TriangularNumberUtil.new }
 
   describe '#get_number' do
-    it 'should return the first triangular number' do
+    it 'returns the first triangular number' do
       expect(util.get_number(1)).to be(1)
     end
 
-    it 'should return the second triangular number' do
+    it 'returns the second triangular number' do
       expect(util.get_number(2)).to be(3)
     end
 
-    it 'should return the third triangular number' do
+    it 'returns the third triangular number' do
       expect(util.get_number(3)).to be(6)
+    end
+
+    it 'returns 0 if given an index of 0' do
+      expect(util.get_number(0)).to be(0)
+    end
+
+    it 'returns 0 if given a negative index' do
+      expect(util.get_number(-5)). to be(0)
     end
 
   end
