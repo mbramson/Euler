f315af58b5c3a31844cad994fc83ac5d073e8def
mbramson <mathewbramson@gmail.com>
2016-04-10 12:44:55 -0400

    ruby problem012 init

---
+++ ruby/problem012/spec/triangular_number_util_spec.rb
@@ -0,0 +1,8 @@
+require 'triangular_number_util'
+
+describe 'triangular number generation' do
+
+
+
+
+end
---
+++ ruby/problem012/spec/world_exists_spec.rb
@@ -0,0 +1,5 @@
+describe 'world_exists' do
+  it 'should exist' do
+    expect(true).to be(true)
+  end
+end
