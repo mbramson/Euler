6ec3f6a164339d45d8f8bf6963fc479165f07153
mbramson <mathewbramson@gmail.com>
2016-04-10 15:32:37 -0400

    refactor divisors_of to only iterate to sqrt n

--- ruby/problem012/lib/divisor_util.rb
+++ ruby/problem012/lib/divisor_util.rb
@@ -2,13 +2,15 @@ class DivisorUtil
 
   def self.divisors_of(n)
     return [] if n <= 0
+    return [1] if n == 1 # to avoid returning [1,1] for n == 1
     divisors = []
-    for i in 1..n do
+    for i in 1..(Math.sqrt(n).floor) do
       if n%i == 0
         divisors.push(i)
+        divisors.push(n/i)
       end
     end
-    divisors
+    divisors.sort
   end
 
   def self.divisor_count_of(n)
