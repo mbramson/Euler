f27043c764b3c71d39b3795e7e59eb85460af5eb
mbramson <mathewbramson@gmail.com>
2016-04-10 13:45:52 -0400

    implement TriangularNumberUtil#get_number

--- ruby/problem012/lib/triangular_number_util.rb
+++ ruby/problem012/lib/triangular_number_util.rb
@@ -1,3 +1,11 @@
 class TriangularNumberUtil
 
+  def get_number(index)
+    sum = 0
+    for i in 1..index
+      sum += i
+    end
+    sum
+  end
+
 end
--- ruby/problem012/spec/triangular_number_util_spec.rb
+++ ruby/problem012/spec/triangular_number_util_spec.rb
@@ -1,8 +1,22 @@
 require 'triangular_number_util'
 
-describe 'triangular number generation' do
+describe TriangularNumberUtil do
 
+  subject(:util) { TriangularNumberUtil.new }
 
+  describe '#get_number' do
+    it 'should return the first triangular number' do
+      expect(util.get_number(1)).to be(1)
+    end
 
+    it 'should return the second triangular number' do
+      expect(util.get_number(2)).to be(3)
+    end
+
+    it 'should return the third triangular number' do
+      expect(util.get_number(3)).to be(6)
+    end
+
+  end
 
 end
