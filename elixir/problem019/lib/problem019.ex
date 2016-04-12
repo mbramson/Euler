f888a1592d1fe8753f408af5b3802d0e742f2263
mbramson <mathewbramson@gmail.com>
2016-04-11 22:37:04 -0400

    implement leap_year?

--- elixir/problem019/lib/problem019.ex
+++ elixir/problem019/lib/problem019.ex
@@ -1,2 +1,8 @@
 defmodule Problem019 do
+
+  def leap_year?(year) do
+    rem(year, 4) == 0 and
+      (rem(year, 400) == 0 or rem(year, 100) != 0)
+  end
+
 end
--- elixir/problem019/test/problem019_test.exs
+++ elixir/problem019/test/problem019_test.exs
@@ -1,8 +1,25 @@
+import Problem019
+
 defmodule Problem019Test do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
   doctest Problem019
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  # leap_year? tests
+
+  test "leap_year? is true in 2012" do
+    assert leap_year? 2012
   end
+
+  test "leap_year? is false in 1995" do
+    refute leap_year? 1995
+  end
+
+  test "leap_year? is true in 2000" do
+    assert leap_year? 2000
+  end
+
+  test "leap_year? is false in 1900" do
+    refute leap_year? 1800
+  end
+
 end
