5e08ed035870d99b395ec5650f86c5d4864cd3b9
mbramson <mathewbramson@gmail.com>
2016-04-12 22:30:52 -0400

    implement days_in

--- elixir/problem019/lib/problem019.ex
+++ elixir/problem019/lib/problem019.ex
@@ -1,8 +1,33 @@
 defmodule Problem019 do
 
+  def days_in(month, year) when is_atom(month) do
+    case month do
+      :january -> 31
+      :february -> days_in_february(year)
+      :march -> 31
+      :april -> 30
+      :may -> 31
+      :june -> 30
+      :july -> 31
+      :august -> 31
+      :september -> 30
+      :october -> 31
+      :november -> 30
+      :december -> 31
+    end
+  end
+
+  defp days_in_february(year) do
+    if leap_year?(year) do
+      29
+    else
+      28
+    end
+  end
+
   def leap_year?(year) do
-    rem(year, 4) == 0 and
-      (rem(year, 400) == 0 or rem(year, 100) != 0)
+    (rem(year, 4) == 0 and rem(year, 100) != 0)
+      or rem(year, 400) == 0
   end
 
 end
--- elixir/problem019/test/problem019_test.exs
+++ elixir/problem019/test/problem019_test.exs
@@ -4,6 +4,60 @@ defmodule Problem019Test do
   use ExUnit.Case, async: true
   doctest Problem019
 
+  # days_in tests
+
+  test "days_in is 31 in January" do
+    assert days_in(:january, 2000) == 31
+  end
+
+  test "days_in is 28 in February for non-leap-years" do
+    assert days_in(:february, 1999) == 28
+  end
+
+  test "days_in is 29 in February on a leap year" do
+    assert days_in(:february, 2004) == 29
+  end
+
+  test "days_in is 31 in March" do
+    assert days_in(:march, 2000) == 31
+  end
+
+  test "days_in is 30 in April" do
+    assert days_in(:april, 2000) == 30
+  end
+
+  test "days_in is 31 in May" do
+    assert days_in(:may, 2000) == 31
+  end
+
+  test "days_in is 30 in June" do
+    assert days_in(:june, 2000) == 30
+  end
+
+  test "days_in is 30 in July" do
+    assert days_in(:july, 2000) == 31
+  end
+
+  test "days_in is 31 in August" do
+    assert days_in(:august, 2000) == 31
+  end
+
+  test "days_in is 30 in September" do
+    assert days_in(:september, 2000) == 30
+  end
+
+  test "days_in is 31 in October" do
+    assert days_in(:october, 2000) == 31
+  end
+
+  test "days_in is 30 in November" do
+    assert days_in(:november, 2000) == 30
+  end
+
+  test "days_in is 31 in December" do
+    assert days_in(:december, 2000) == 31
+  end
+
   # leap_year? tests
 
   test "leap_year? is true in 2012" do
