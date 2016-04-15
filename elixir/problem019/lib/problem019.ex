cb3c8a90c4958908321c4d75dfcfdfc6269fd001
mbramson <mathewbramson@gmail.com>
2016-04-14 22:05:20 -0400

    add documentation to problem019

--- elixir/problem019/lib/problem019.ex
+++ elixir/problem019/lib/problem019.ex
@@ -1,11 +1,24 @@
 defmodule Problem019 do
 
 
+  @doc """
+  Returns the numerical difference between the first day and the second day of
+  the same month and year. In the case that first_day = second_day this
+  returns 0.
+
+  Example: day_diff(%{year: 2000, month: 1, day: 1},
+                    %{year: 2000, month: 1, day: 10})
+           will return 9.
+  """
   def day_diff(%{year: same_year, month: same_month, day: first_day},
                %{year: same_year, month: same_month, day: second_day}) do
     second_day - first_day
   end
 
+  @doc """
+  Returns the numerical difference in days between the first date and the
+  second date when the first and second date are in the same year.
+  """
   def day_diff(%{year: same_year, month: first_month, day: first_day},
                %{year: same_year, month: second_month, day: second_day}) do
     days_left_in_first_month =
@@ -22,11 +35,25 @@ defmodule Problem019 do
 
   end
 
+  @doc """
+  Returns the numerical difference in days between the first date and the
+  second date when the first and second date are in different years.
+  """
   def day_diff(%{year: first_year,  month: first_month,  day: first_day},
                %{year: second_year, month: second_month, day: second_day}) do
     nil # TODO: Implement
   end
 
+  @doc """
+  Returns the number of days in the months between the first_month and the
+  second_month parameters in the given year. Takes into account leap years
+  when the year is a leap year and one of the months being summed is February.
+
+  Example: days_in_months_between(5, 8, 1998) will return:
+    number of days in June (30) +
+    number of days in July (31)
+    = 61.
+  """
   def days_in_months_between(first_month, second_month, year)
         when second_month > first_month + 1 do
     Enum.reduce(first_month+1..second_month-1, 0,
@@ -35,6 +62,9 @@ defmodule Problem019 do
 
   def days_in_months_between(first_month, second_month, year), do: 0
 
+  @doc """
+  Returns the number days in the given month on the given year.
+  """
   def days_in(%{year: year, month: month}) do
     cond do
       month in [1,3,5,7,8,10,12] -> 31
@@ -44,6 +74,9 @@ defmodule Problem019 do
     end
   end
 
+  @doc """
+  Returns the number of days in a given year
+  """
   def days_in(%{year: year}) do
     case leap_year?(year) do
       false -> 365
@@ -58,6 +91,9 @@ defmodule Problem019 do
     end
   end
 
+  @doc """
+  Returns true if the given year is a leap year
+  """
   def leap_year?(year) do
     (rem(year, 4) == 0 and rem(year, 100) != 0)
       or rem(year, 400) == 0
