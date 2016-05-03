2db56c7a2f087b7a244d58320a643ebf7e3d40c4
mbramson <mathewbramson@gmail.com>
2016-05-03 10:58:36 -0400

    implement incomplete score_word

--- elixir/problem022/lib/problem022.ex
+++ elixir/problem022/lib/problem022.ex
@@ -1,3 +1,41 @@
 defmodule Problem022 do
+
+
+  @doc """
+  Streams in the given file and produces a map for each word in the file where
+  the key is the name score. 
+  
+  The name score of a word is the sum of the corresponding alphabet position 
+  for each letter (a is 1, c is 3, etc.) multiplied by the words overall 
+  alphabetical position among all other words in the source file.
+  
+  TODO: IMPLEMENT
+  """
+  def process_names(file) do
+    File.open!(file)
+    |> IO.stream(:line)
+    |> IO.inspect
+  end
+
+  @doc """
+  Returns the sum of the corresponding alphabet position for each letter in
+  the word parameter.
+
+  Example:
+  score_word("cat") is 24 (3 + 1 + 20)
+  """
+  def score_word(word) do
+    word
+    |> String.codepoints
+    |> Enum.reduce(0, fn l, acc -> acc + single_letter_score!(l) end)
+  end
+
+  defp single_letter_score!(letter) do
+    case letter do
+      "" -> 0
+      "a" -> 1
+      _ -> raise "invalid character for single_letter_score"
+    end
+  end
   
 end
--- elixir/problem022/test/problem022_test.exs
+++ elixir/problem022/test/problem022_test.exs
@@ -1,8 +1,21 @@
+import Problem022
+
 defmodule Problem022Test do
   use ExUnit.Case
   doctest Problem022
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  # base_score tests
+
+  test "empty string scores 0" do
+    assert score_word("") == 0
   end
+
+  test "a scores 1" do
+    assert score_word("a") == 1
+  end
+
+  test "aa scores 2" do
+    assert score_word("aa") == 2
+  end
+
 end
