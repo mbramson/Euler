67fb79431ec437b93b2645f31538f16500ffc874
mbramson <mathewbramson@gmail.com>
2016-05-23 21:20:54 -0400

    add generate_order_map

--- elixir/problem024/lib/problem024.ex
+++ elixir/problem024/lib/problem024.ex
@@ -1,9 +1,28 @@
 defmodule LexicographicPermutation do
   
+  @doc """
+  Returns all permutations of the items in the given list in lexicographic
+  order where the resulting lexicographic order is determined by the order of
+  the elements in the input list.
+  """
   def ordered_permute([]), do: []
-  def ordered_permute([a]), do: [a]
+  def ordered_permute([a]) when is_integer(a), do: [Integer.to_string(a)]
+  def ordered_permute([a]) when is_binary(a), do: [a]
   def ordered_permute(members) do
     []
+  end
+  
+  @doc """
+  Generates a map where the keys are the elements of the original list and the
+  values are their order in the original list.
+  """
+  def generate_order_map(list) when is_list(list) do 
+    _generate_order_map(list, 1)
+    |> Enum.into(%{})
+  end
+  defp _generate_order_map([], _), do: []
+  defp _generate_order_map([head|tail], index) do
+    [{head, index}] ++ _generate_order_map(tail, index+1)
   end
 
 end
--- elixir/problem024/test/problem024_test.exs
+++ elixir/problem024/test/problem024_test.exs
@@ -8,8 +8,32 @@ defmodule LexicographicPermutationTest do
     assert ordered_permute([]) == []
   end
 
-  test "permutation of single element list is that element" do
-    assert ordered_permute([1]) == [1]
+  test "permutation of single element list is that element as string" do
+    assert ordered_permute([1]) == ["1"]
   end
+
+  test "permutation of single element list is that element" do
+    assert ordered_permute(["c"]) == ["c"]
+  end
+
+  @tag :skip
+  test "two element permutation" do
+    assert ordered_permute([0, 1]) == ["01", "10"]
+  end
+
+  # generate_order_map tests
+
+  test "order map of no element list is empty map" do
+    assert generate_order_map([]) == %{}
+  end
+
+  test "order map of one element list is correct" do
+    assert generate_order_map(["C"]) == %{"C" => 1}
+  end
+
+  test "order map of two element list is correct" do
+    assert generate_order_map(["G", "F"]) == %{"G" => 1, "F" => 2}
+  end
+
 
 end
