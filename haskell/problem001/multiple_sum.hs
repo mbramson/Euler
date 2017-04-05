e2ef624f5fb52588ec867cd8364bf69beb0104dc
Mathew Bramson <mathewbramson@gmail.com>
2017-04-04 22:19:37 -0400

    Solve Haskell problem 1

--- haskell/problem001/multiple_sum.hs
+++ haskell/problem001/multiple_sum.hs
@@ -1,1 +1,11 @@
+import Data.List
+
+sum_multiples :: (Foldable t, Integral a) => t a -> a -> a
+sum_multiples divisors limit = sum $ filter (\x -> multipleOfAny x divisors) [1..limit-1]
+
+multipleOfAny :: (Integral a, Foldable t) => a -> t a -> Bool
+multipleOfAny n divisors = any (\x -> multipleOf x n) divisors
+
+multipleOf :: Integral a => a -> a -> Bool
+multipleOf divisor n = n `mod` divisor == 0
 
